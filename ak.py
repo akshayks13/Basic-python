de94b10e4a10768fd85cfc5de14bd1c140b9c2b7
akshayks13 <akshayks1005@gmail.com>
2023-10-16 15:37:00 +0530

    first sum

---
+++ ak.py
@@ -0,0 +1,4 @@
+print("hello")
+a=int(input())
+b=2*a
+print(b)
---
+++ code1.py
@@ -0,0 +1,8 @@
+num1=int(input("Enter first number:"))
+num2=int(input("Enter first number:"))
+
+for i in range(num1,num2):
+    if i%2==0:
+        print(i,"is even")
+    else:
+        print(i,"is odd")
