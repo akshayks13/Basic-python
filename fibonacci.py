527bb568fd168e368f4b607fe36870f51aca5662
akshayks13 <143313500+akshayks13@users.noreply.github.com>
2023-11-06 11:05:21 +0530

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,16 @@
+#To print the fibonacci series till the user specified number
+
+n=int(input("Enter the number:"))
+if (n<=0):
+    print("enter a valid number")
+elif (n==1):
+    print(1,end=' ')
+else:
+    a=0
+    print(a,end=' ')
+    b=1
+    for i in range(n):
+        print(b,end=' ')
+        s=a+b
+        a=b
+        b=s
